1546ae1dfc8fa5dd19f524141f8b7c4b33e38b7b
Raghuvamsi Ayapilla <vamsi.ar@gmail.com>
2024-04-18 06:59:57 -0400

    update requirements file to include basic libraries, update load.py file to print ames data

--- files/JB_ds_beginner_book_4_pramoda/beginners_guide_data_science_code/chapter_01/01_load.py
+++ files/JB_ds_beginner_book_4_pramoda/beginners_guide_data_science_code/chapter_01/01_load.py
@@ -1,6 +1,8 @@
 # Load the Ames dataset
 import pandas as pd
-Ames = pd.read_csv('Ames.csv')
+Ames = pd.read_csv('/Users/vamsi_mbmax/Library/CloudStorage/OneDrive-Personal/01_vam_PROJECTS/SIDE_PROJECTS/proj_Pramoda_python/subproj_jb_datascience/files/JB_ds_beginner_book_4_pramoda/beginners_guide_data_science_code/chapter_01/Ames.csv')
+print(Ames)
+# Ames = pd.read_csv('Ames.csv')
 
 # Dataset shape
 print(Ames.shape)
